ecd266361033884e7bcdfec6366c713e0ab1f93b
Ankit <ankitmhatre.mhatre9@gmail.com>
2023-01-08 20:03:38 -0500

    Initial Commit

---
+++ Fancaster/FancasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FancasterApp.swift
+//  Fancaster
+//
+//  Created by Ankit Mhatre on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct FancasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
